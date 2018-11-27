e7109eb0c8c8eb8012a9ba6ab197989677c8d030
mads <pihalve@gmail.com>
2018-11-27 23:58:17 +0100

    client app refactoring

--- Site/client-app/src/App.js
+++ Site/client-app/src/App.js
@@ -46,39 +46,32 @@ class Textbox extends React.Component {
 class Dropdown extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {selected: props.selected};
+    // this.state = {selected: props.selected};
     this.handleChange = this.handleChange.bind(this);
-    // this.toggle = this.toggle.bind(this);
-    // this.state = {isOpen: false};
   }
   handleChange(event) {
-    console.log('Dropdown says: ' + event.value);
-    this.setState({selected: event.target.value});
+    console.log('Dropdown says: ' + event.target.value);
+    // this.setState({selected: event.target.value});
     this.props.onChange(event.target.value);
   }
-  setSelected(item) {
-    return item === undefined || item === null || item === '' ? 'Options' : item;
-  }
-  // toggle() {
-  //   this.setState({
-  //     isOpen: !this.state.isOpen
-  //   });
+  // setSelected(item) {
+  //   return item === undefined || item === null || item === '' ? 'Options' : item;
   // }
   render() {
-    /* <select value={this.state.value} onChange={this.handleChange}>
-      {this.props.list.map((item) => <option key={item} value={item}>{item}</option>)}
-    </select> */
     return (
-      <UncontrolledDropdown size='sm' style={{width:'100%'}}>
-        <DropdownToggle caret style={{width:'100%'}}>
-            {this.setSelected(this.state.selected)}
-        </DropdownToggle>
-        <DropdownMenu style={{width:'100%'}}>
-          {this.props.list.map((item) => 
-            <DropdownItem key={item} value={item} onClick={this.handleChange}>{item}</DropdownItem>
-          )}
-        </DropdownMenu>
-      </UncontrolledDropdown>
+      <Input type='select' value={this.props.selected} onChange={this.handleChange}>
+        {this.props.list.map((item) => <option key={item} value={item}>{item}</option>)}
+      </Input>
+      // <UncontrolledDropdown size='sm' style={{width:'100%'}}>
+      //   <DropdownToggle caret style={{width:'100%'}}>
+      //       {this.setSelected(this.state.selected)}
+      //   </DropdownToggle>
+      //   <DropdownMenu style={{width:'100%'}}>
+      //     {this.props.list.map((item) => 
+      //       <DropdownItem key={item} value={item} onClick={this.handleChange}>{item}</DropdownItem>
+      //     )}
+      //   </DropdownMenu>
+      // </UncontrolledDropdown>
     );
   }
 }
@@ -86,25 +79,42 @@ class Dropdown extends React.Component {
 class Rule extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      field: this.props.rule.field,
+      operator: this.props.rule.operator,
+      value: this.props.rule.value
+    };
     this.handleFieldChange = this.handleFieldChange.bind(this);
     this.handleOperatorChange = this.handleOperatorChange.bind(this);
     this.handleValueChange = this.handleValueChange.bind(this);
   }
   handleFieldChange(event) {
-    this.props.onChange({field: event});
+    const ruleId = this.props.rule.id;
+    const name = 'field';
+    const value = event;
+    this.setState({field: value});
+    this.props.onChange({ruleId, name, value});
   }
   handleOperatorChange(event) {
-    this.props.onChange({operator: event});
+    const ruleId = this.props.rule.id;
+    const name = 'operator';
+    const value = event;
+    this.setState({operator: value});
+    this.props.onChange({ruleId, name, value});
   }
   handleValueChange(event) {
-    this.props.onChange({value: event});
+    const ruleId = this.props.rule.id;
+    const name = 'value';
+    const value = event;
+    this.setState({value: value});
+    this.props.onChange({ruleId, name, value});
   }
   render() {
     return (
       <Row>
-        <Col sm='3'><Dropdown list={this.props.fields} selected={this.props.rule.field} onChange={this.handleFieldChange} /></Col>
-        <Col sm='3'><Dropdown list={this.props.operators} selected={this.props.rule.operator} onChange={this.handleOperatorChange} /></Col>
-        <Col sm='6'><Textbox value={this.props.rule.value} onChange={this.handleValueChange} /></Col>
+        <Col xs='3'><Dropdown list={this.props.fields} selected={this.state.field} onChange={this.handleFieldChange} /></Col>
+        <Col xs='3'><Dropdown list={this.props.operators} selected={this.state.operator} onChange={this.handleOperatorChange} /></Col>
+        <Col xs='6'><Textbox value={this.state.value} onChange={this.handleValueChange} /></Col>
       </Row>
     );
   }
@@ -115,19 +125,27 @@ class RuleList extends React.Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.state = {rules: [
-      {id: 1, field: 'Year', operator: 'is', value: '2018'},
-      {id: 2, field: 'Artist', operator: 'starts with', value: ''},
-      {id: 3, field: 'Comments', operator: 'contains', value: ''}]};
+      {id: 0, field: 'Year', operator: 'is', value: '2018'},
+      {id: 1, field: 'Artist', operator: 'starts with', value: ''},
+      {id: 2, field: 'Comments', operator: 'contains', value: ''}]};
   }
   handleChange(event) {
-    console.log('RuleList says: ' + event.value);
+    //console.log('RuleList says: ' + event.field + '|' + event.operator + '|' + event.value);
+    console.log('Rulelist says: ' + event.ruleId + ':' + event.name + '=' + event.value);
+    const rules = this.state.rules;
+    rules[event.ruleId][event.name] = event.value;
+    //this.setState({[event.name]: event.value});
+    this.setState({rules: rules});
     //this.props.onChange({value: event.value});
   }
   render () {
     return (
       <FormGroup>
         {this.state.rules.map((rule) => 
-          <Rule key={rule.id} fields={this.props.fields} operators={this.props.operators} rule={rule} onChange={this.handleChange} />,
+          <div>
+            <Rule key={rule.id} fields={this.props.fields} operators={this.props.operators} rule={rule} onChange={this.handleChange} />
+            <span>{this.state.rules[rule.id].field}</span>
+          </div>,
           this
         )}
       </FormGroup>
@@ -157,22 +175,22 @@ class App extends Component {
         </Navbar>
         <Form>
           <Row>
-            <Col sm='3'></Col>
-            <Col sm='3'><Textbox id='playlistName' label='Playlist name' onChange={this.handlePlaylistNameChange} /></Col>
-            <Col sm='6'></Col>
+            <Col xs='3'></Col>
+            <Col xs='3'><Textbox id='playlistName' label='Playlist name' onChange={this.handlePlaylistNameChange} /></Col>
+            <Col xs='6'></Col>
           </Row>
           <Row>
-            <Col sm='3'></Col>
-            <Col sm='6'><RuleList fields={mp3Tags} operators={operators} /></Col>
-            <Col sm='3'></Col>
+            <Col xs='3'></Col>
+            <Col xs='6'><RuleList fields={mp3Tags} operators={operators} /></Col>
+            <Col xs='3'></Col>
           </Row>
           <Row>
-            <Col sm='3'></Col>
-            <Col sm='6'>
+            <Col xs='3'></Col>
+            <Col xs='6'>
               <Button>Save</Button>
               <Button style={{marginLeft:'5px'}}>Cancel</Button>
             </Col>
-            <Col sm='3'></Col>
+            <Col xs='3'></Col>
           </Row>
         </Form>
       </div>
